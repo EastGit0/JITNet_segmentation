0d5ce0cc5ab95338612c50aec7346481387260e6
East <eastgit0@gmail.com>
2021-05-30 12:26:26 -0700

    train jitnet on cpu

--- base/base_trainer.py
+++ base/base_trainer.py
@@ -85,6 +85,7 @@ class BaseTrainer:
 
         if resume: self._resume_checkpoint(resume)
 
+        print("My device is ",self.device)
         if self.device == 'cpu':
             torch.set_flush_denormal(True)
             print("Flush Denormals Enabled")
