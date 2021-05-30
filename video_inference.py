7a81b2f70d0479fd0e7aadb73d286d7ff9a6ab0e
East <eastgit0@gmail.com>
2021-05-29 18:07:30 -0700

    test live video

--- video_inference.py
+++ video_inference.py
@@ -47,48 +47,46 @@ def worker_stream():
         print("Running JITNet on CPU!")
 
 
-     # Dataset used for training the model
-    # dataset_type = config['train_loader']['type']
-    # assert dataset_type in ['DETECTRON', 'COCO']
-    # scales = [0.75, 1.0, 1.25, 1.5, 1.75, 2.0]
-    # loader = getattr(dataloaders, config['train_loader']['type'])(**config['train_loader']['args'])
-    # to_tensor = transforms.ToTensor()
-    # normalize = transforms.Normalize(loader.MEAN, loader.STD)
-    # num_classes = loader.dataset.num_classes
-    # palette = loader.dataset.palette
+    # Dataset used for training the model
+    config = json.load(open("config_detectron.json"))
+    dataset_type = config['train_loader']['type']
+    assert dataset_type in ['DETECTRON', 'COCO']
+    scales = [0.75, 1.0, 1.25, 1.5, 1.75, 2.0]
+    loader = getattr(dataloaders, config['train_loader']['type'])(**config['train_loader']['args'])
+    to_tensor = transforms.ToTensor()
+    normalize = transforms.Normalize(loader.MEAN, loader.STD)
+    num_classes = loader.dataset.num_classes
+    palette = loader.dataset.palette
 
-    # # Model
-    # model = getattr(models, config['arch']['type'])(num_classes, **config['arch']['args'])
-    # availble_gpus = list(range(torch.cuda.device_count()))
-    # device = torch.device('cuda:0' if len(availble_gpus) > 0 else 'cpu')
+    # Model
+    model = getattr(models, config['arch']['type'])(num_classes, **config['arch']['args'])
+    availble_gpus = list(range(torch.cuda.device_count()))
+    device = torch.device('cuda:0' if len(availble_gpus) > 0 else 'cpu')
 
-    # checkpoint = torch.load(args.model, map_location=device)
-    # #print("Checkpoint: ", checkpoint)
-    # if isinstance(checkpoint, dict) and 'state_dict' in checkpoint.keys():
-    #     checkpoint = checkpoint['state_dict']
-    #     #print("Update state dict: ", checkpoint)
-    # if 'module' in list(checkpoint.keys())[0] and not isinstance(model, torch.nn.DataParallel):
-    #     model = torch.nn.DataParallel(model)
-    #     #print("Update parallel: ", model)
-    # model.load_state_dict(checkpoint, strict=False)
-    # model.to(device)
-    # model.eval()
+    checkpoint = torch.load("./saved/pretrained_models/checkpoint-epoch9.pth", map_location=device)
+    #print("Checkpoint: ", checkpoint)
+    if isinstance(checkpoint, dict) and 'state_dict' in checkpoint.keys():
+        checkpoint = checkpoint['state_dict']
+        #print("Update state dict: ", checkpoint)
+    if 'module' in list(checkpoint.keys())[0] and not isinstance(model, torch.nn.DataParallel):
+        model = torch.nn.DataParallel(model)
+        #print("Update parallel: ", model)
+    model.load_state_dict(checkpoint, strict=False)
+    model.to(device)
+    model.eval()
 
-    # if not os.path.exists('outputs'):
-    #     os.makedirs('outputs')
+    image_files = sorted(glob(os.path.join(args.images, f'*.{args.extension}')))
+    with torch.no_grad():
+        tbar = tqdm(image_files, ncols=100)
+        for img_file in tbar:
+            image = Image.open(img_file).convert('RGB')
+            input = normalize(to_tensor(image)).unsqueeze(0)
 
-    # image_files = sorted(glob(os.path.join(args.images, f'*.{args.extension}')))
-    # with torch.no_grad():
-    #     tbar = tqdm(image_files, ncols=100)
-    #     for img_file in tbar:
-    #         image = Image.open(img_file).convert('RGB')
-    #         input = normalize(to_tensor(image)).unsqueeze(0)
+            prediction = model(input.to(device))
+            prediction = prediction[0].squeeze(0).cpu().numpy()
 
-    #         prediction = model(input.to(device))
-    #         prediction = prediction[0].squeeze(0).cpu().numpy()
-
-    #         prediction = F.softmax(torch.from_numpy(prediction), dim=0).argmax(0).cpu().numpy()
-    #         save_images(image, prediction, args.output, img_file, palette)
+            prediction = F.softmax(torch.from_numpy(prediction), dim=0).argmax(0).cpu().numpy()
+            save_images(image, prediction, args.output, img_file, palette)
 
 
 
@@ -110,12 +108,19 @@ def worker_stream():
     while True:
         for im in s:
             assert im is not None
+
+            input = normalize(to_tensor(im.convert('RGB'))).unsqueeze(0)
+
+            prediction = model(input.to(device))
+            prediction = prediction[0].squeeze(0).cpu().numpy()
+            prediction = F.softmax(torch.from_numpy(prediction), dim=0).argmax(0).cpu().numpy()
+
             # if frame_id == 0:
             #     window = ax1.imshow(im)
             #     plt.ion()
             # Using cv2.imshow() method 
             # Displaying the image 
-            cv2.imshow(window_name, im)
+            cv2.imshow(prediction, im)
             cv2.waitKey(20)
 
 
