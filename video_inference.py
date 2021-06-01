948dcc8d5e7c0dfd78717af76f66083795996b45
East <eastgit0@gmail.com>
2021-05-31 17:37:14 -0700

    debug

--- video_inference.py
+++ video_inference.py
@@ -116,8 +116,8 @@ class Student():
                 ##### Display new Frame #####
                 prediction[np.where(prediction >= 1)] = 255
                 im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-                cv2.imshow(self.window_name, np.add(im, prediction).astype(np.uint8)) #prediction.astype(np.uint8)
-                
+                # cv2.imshow(self.window_name, np.add(im, prediction).astype(np.uint8)) #prediction.astype(np.uint8)
+                cv2.imshow(self.window_name, prediction.astype(np.uint8))
 
                 ##### Check for New Weights #####
                 if os.path.exists(self.next_weight_path):
