41c59a320729bb1b20cad159f8248e48ff56d3c1
East <eastgit0@gmail.com>
2021-06-02 14:15:06 -0700

    debug

--- student.py
+++ student.py
@@ -138,7 +138,11 @@ class Student():
                 cv2.imshow(self.window_name, im) #prediction.astype(np.uint8)
 
                 ##### Check for New Weights #####
-                self.scp.get(local_path=self.next_weight_path, remote_path=("/home/cs348k/data/student/weights/{}/weights_{}".format(self.config['arch']['type'], str(self.next_weight_id))))
+                try:
+                    self.scp.get(local_path=self.next_weight_path, remote_path=("/home/cs348k/data/student/weights/{}/weights_{}".format(self.config['arch']['type'], str(self.next_weight_id))))
+                except SCPClient.SCPException:
+                    pass
+
                 if os.path.exists(self.next_weight_path):
                   self.load_weights(self.next_weight_path)
                   os.system("rm {}".format(self.next_weight_path))
