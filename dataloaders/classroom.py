e14c1cd12e5a56f15c1d6f0ed66e1e982a6f5359
East <eastgit0@gmail.com>
2021-06-02 11:41:51 -0700

    debug

--- dataloaders/classroom.py
+++ dataloaders/classroom.py
@@ -23,7 +23,8 @@ class ClassroomStuff(BaseDataSet):
     def _set_files(self):
         # if self.split in ['train', 'val']:
         file_list = sorted(glob(os.path.join(self.root, 'frames/frame_', '*.jpg')))
-        self.files = [os.path.basename(f).split('.')[0][6:] for f in file_list]
+        self.files = [os.path.basename(f).split('.')[0][13:] for f in file_list]
+        print(self.files)
         # else: raise ValueError(f"Invalid split name {self.split}, either train2017 or val2017")
 
     def _load_data(self, index):
