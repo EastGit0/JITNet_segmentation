52445de80d52e71df0667d9e003bc59a51258eaf
East <eastgit0@gmail.com>
2021-06-02 18:45:10 -0700

    debug

--- student.py
+++ student.py
@@ -139,7 +139,7 @@ class Student():
                 print(prediction[1,:,:])
                 cv2.imshow("Person", (prediction[1,:,:] * 255).numpy())
                 cv2.imshow("Background", (prediction[0,:,:]).numpy())
-                cv2.imshow("Summed", (prediction[0,:,:] + sum(prediction[2:,:,:], axis=0)).numpy())
+                cv2.imshow("Summed", (prediction[0,:,:] + torch.sum(prediction[2:,:,:], dim=0)).numpy())
                 prediction = prediction.argmax(0)
                 print(prediction)
                 prediction = prediction.numpy()
